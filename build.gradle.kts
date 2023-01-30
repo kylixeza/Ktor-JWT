64902cb58d717713d76d8916b6e2594053ea4f72
ACER <loilyxtra@gmail.com>
2023-01-30 20:47:59 +0700

    added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val postgresql_version: String by project
+val hikari_version: String by project
+val koin_version: String by project
+
 
 plugins {
     kotlin("jvm") version "1.8.0"
@@ -31,4 +36,21 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    //Exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+
+    //Postgresql
+    implementation("org.postgresql:postgresql:$postgresql_version")
+
+    //Hikari
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+
+    //DI
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+
+    implementation("commons-codec:commons-codec:1.15")
 }
